834e2cf33152af3185999dc31bf8f5c540063fac
magodi20 <manolo.2000@outlook.es>
2021-04-03 17:48:10 +0200

    Refactor addNewQuote to use Async/await

--- script.js
+++ script.js
@@ -15,26 +15,13 @@ const QUOTE_URL = "https://goquotes-api.herokuapp.com/api/v1/random?count=1";
 
 
 
-function addNewQuote() {
-    const promise = fetch(QUOTE_URL);
-    promise
-        .then((response) => {
-            return response.json();
+async function addNewQuote() {
+    const promise = await fetch(QUOTE_URL);
+    const json = await promise.json()
 
-        })
-
-        .then(function (response) {
-            text.innerHTML = response.quotes[0].text;
-            author.innerHTML = response.quotes[0].author;
-            tweetQuotelink.href = `https://twitter.com/intent/tweet?text=${encodeURI(`"${text.innerHTML}" - ${author.innerHTML}`)}`
-
-
-        })
-
-
-
-
-
+    text.innerHTML = json.quotes[0].text;
+    author.innerHTML = json.quotes[0].author;
+    tweetQuotelink.href = `https://twitter.com/intent/tweet?text=${encodeURI(`"${text.innerHTML}" - ${author.innerHTML}`)}`
 
 }
 
