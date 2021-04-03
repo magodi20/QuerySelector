013ccb4ea2c802306b02d2a6b575bf50a99ef99c
magodi20 <manolo.2000@outlook.es>
2021-04-03 17:05:08 +0200

    Implement US# 5, 6, 7, 8, 9 ... Implemented functionality to get Quote information.

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 let newQuoteButton = document.getElementById('new-quote');
+let text = document.getElementById('text');
+let author = document.getElementById('author');
+
+newQuoteButton.addEventListener('click', (e) => {
+    addNewQuote();
+})
+
+
+
+const QUOTE_URL = "https://goquotes-api.herokuapp.com/api/v1/random?count=1";
+
+
+
+
+function addNewQuote() {
+    const promise = fetch(QUOTE_URL);
+    promise
+        .then((response) => {
+            return response.json();
+
+        })
+
+        .then(function (response) {
+            text.innerHTML = response.quotes[0].text;
+            author.innerHTML = response.quotes[0].author;
+
+
+        })
+
+
+
+
+
+
+}
+
+addNewQuote()
+
+
+
